949a4399e2d3246bec9f4f1b474ecee1d9c38f4a
Bryan <brayancan0928@hotmail.com>
2018-11-08 16:44:52 -0500

    alias
    se creo alias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'progresiveweb';
+  constructor(private swUpdate:SwUpdate ){
+
+  }
+  ngOnInit(): void{
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe( next: () => {
+        window.location.reload();
+      })
+    }
+  }
 }
